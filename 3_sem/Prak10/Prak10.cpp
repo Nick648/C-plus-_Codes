fe357a1a8963256750031baf23bfc3d1c91a6847
Manokhin Dmitry <90748795+Nick648@users.noreply.github.com>
2021-11-08 21:24:17 +0300

    Add files via upload

---
+++ 3_sem/Prak10/Prak10.cpp
@@ -0,0 +1,33 @@
+﻿// Prak10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int min(int* arr, int n, int b) {
+    if (arr[n] < b)
+        b = arr[n];
+    if (n == 0)
+        return b;
+    return min(arr, n - 1, b);
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите кол-во элементов: ";
+    int n;
+    cin >> n;
+    if (n <= 0) { cout << "Error"; return 0; }
+    int* arr = new int[n];
+    cout << "Введите массив: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int b = arr[n - 1];
+    b = min(arr, n - 1, b);
+    cout << "Минимальное значение = " << b << endl;
+    return 0;
+}
