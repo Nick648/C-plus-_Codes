418eb20e7f157e1af4bdca9e1b16bb9d098d540f
Manokhin Dmitry <90748795+Nick648@users.noreply.github.com>
2021-11-23 23:22:04 +0300

    Add files via upload

---
+++ 3_sem/Prak4/Prak4.cpp
@@ -0,0 +1,28 @@
+﻿// Prak4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string s;
+	cout << "Введите email: ";
+	cin >> s;
+	int k = s.find('@');
+	if (s.substr(k) != "@mail.ru" and s.substr(k) != "@cs.mcgill.ca" and
+		s.substr(k) != "@mtu-shet.info") {
+		cout << "Wrong email!";
+	}
+	else {
+		s.erase(k);
+		if (isalpha(s[0]) and isalnum(s[s.size() - 1])) {
+			cout << "Correct email!" << endl;
+		}
+		else {
+			cout << "Wrong email!" << endl;
+		}
+	}
+	return 0;
+}
