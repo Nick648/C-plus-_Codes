381d5fb1a8ff00f732ad220a0bbf6aeab5a1b347
Manokhin Dmitry <90748795+Nick648@users.noreply.github.com>
2021-11-23 23:25:48 +0300

    Add files via upload

---
+++ 3_sem/Prak5/Prak5.cpp
@@ -0,0 +1,40 @@
+﻿// Prak5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <conio.h>
+#include <iostream>
+#include <string.h>
+#include <ctype.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int k;
+	string chis = "";
+	char raz[] = " ,.!?:;-";
+	char s[100];
+	char* word;
+	cout << "Ввод текста: ";
+	cin.getline(s, 100);
+	word = strtok(s, raz);
+	while (word != 0)
+	{
+		k = 1;
+		for (int i = 0; i < strlen(word); i++) {
+			if (isdigit(word[i]) == 0) {
+				k = 0;
+			}
+		}
+		if (k == 1 and strlen(word) > chis.size()) {
+			chis = word;
+		}
+		word = strtok(0, raz);
+	}
+	if (chis == "") {
+		cout << "Слов чисел в тексте нет!";
+	}
+	else {
+		cout << "Максимальное слово число: " << chis;
+	}
+	return 0;
+}
