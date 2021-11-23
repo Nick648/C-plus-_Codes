fc4e94cb7073a85f93ab16a4c22dba692385de96
Manokhin Dmitry <90748795+Nick648@users.noreply.github.com>
2021-11-23 23:06:41 +0300

    Add files via upload

---
+++ 3_sem/Prak1/Prak1.cpp
@@ -0,0 +1,27 @@
+﻿// Prak1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+// 5 вариант
+using namespace std;
+
+/*ГЛАВНАЯ ФУНКЦИЯ*/
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x, y, r, a;
+	cout << "Радиус окружности и сторона квадрата: ";
+	cin >> r >> a;
+	cout << "Координаты x, y: ";
+	cin >> x >> y;
+	if (a * sqrt(2) <= 2 * r) {
+		if (y <= sqrt(r * r - x * x) && abs(x) >= a / 2 && abs(y) >= a / 2)
+			cout << "YES";
+		else
+			cout << "NO";
+	}
+	else
+		cout << "Неверный рисунок";
+	return 0;
+}
